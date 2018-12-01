7a654586e3a7ea139b8c60071f6e65033b0a1899
Denis Craig <dcraig@thousandeyes.com>
2018-12-01 18:59:29 +0000

    Simplify axios request

--- src/notifier/index.js
+++ src/notifier/index.js
@@ -3,34 +3,6 @@
 const AWS = require('aws-sdk');
 const axios = require('axios');
 
-const getPassword = async () => {
-    return 'changeme';
-    return new Promise((resolve, reject) => {
-        const client = new AWS.SecretsManager({
-            region: 'eu-west-2',
-        });
-    
-        client.getSecretValue({
-            SecretId: 'sourceforts/rcon',
-        }, (err, data) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-    
-            if ('SecretString' in data) {
-                secret = data.SecretString;
-                resolve(secret);
-                return;
-            }
-
-            let buff = new Buffer(data.SecretBinary, 'base64');
-            decodedBinarySecret = buff.toString('ascii');
-            resolve(decodedBinarySecret);
-        });
-    });
-};
-
 const getAddresses = async region => {
     console.log(`Getting elastic ips for region ${region}`);
     const ec2 = new AWS.EC2({
@@ -51,8 +23,7 @@ const getAddresses = async region => {
     });
 }
 
-exports.handler = async (event, context, callback) => {
-    const password = await getPassword();
+exports.handler = async (event, context) => {
     const addresses = [
         ...(await getAddresses('eu-west-2')),
         ...(await getAddresses('us-east-1')),
@@ -65,13 +36,10 @@ exports.handler = async (event, context, callback) => {
     console.log(`Update: ${message}`);
 
     addresses.forEach(async addr => {
-        axios.post(`${addr}/api/v1/command`, {
-            command: `say [BOT] New server version detected. ${message}`,
-            password,
-        })
-        .then(console.log)
-        .catch(err => {
-            throw err;
-        });
+        try {
+            await axios.post(`${addr}/api/v1/request-update`, { message });
+        } catch (error) {
+            context.done(null, error);
+        }
     });
 };
