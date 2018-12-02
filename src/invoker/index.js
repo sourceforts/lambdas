dcfe125bb5eee09bef555b3b1a493c8722c8fc37
Denis Craig <dcraig@thousandeyes.com>
2018-12-02 21:26:22 +0000

    nop, dont escape me

--- src/invoker/index.js
+++ src/invoker/index.js
@@ -11,7 +11,7 @@ exports.handler = async (event, context) => {
     };
 
     try {
-        await axios.post('https://api.travis-ci.org/repo/sourceforts\%\2Fserver/requests', {
+        await axios.post('https://api.travis-ci.org/repo/sourceforts%2Fserver/requests', {
             request: {
                 branch: 'master',
             },
