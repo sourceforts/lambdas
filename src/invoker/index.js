4c31351089d5bd8d9d0816191eae713960d851ba
Denis Craig <dcraig@thousandeyes.com>
2018-12-02 00:56:43 +0000

    Add invoker functionality

--- src/invoker/index.js
+++ src/invoker/index.js
@@ -1,5 +1,25 @@
 'use strict';
 
+const axios = require('axios');
+
 exports.handler = async (event, context) => {
+    const config = {
+        headers: {
+            Authorization: `token ${process.env.TRAVIS_TOKEN}`,
+            'Travis-API-Version': 3,
+        },
+    };
+
+    try {
+        await axios.post('https://api.travis-ci.org/repo/sourceforts/server/requests', {
+            request: {
+                branch: 'master',
+            },
+        }, config);   
+    } catch (error) {
+        console.log(error);
+        context.done(null, error);
+    }
+
     console.log('Done!');
 };
